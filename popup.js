0cd41363d7ed5c27f6ebcfe439cc7f9df9e3270b
King W T <king.tkw.97@gmail.com>
2022-03-01 19:13:01 +1100

    Added chrome.storage; the popup now remembers the values on any close or open. All buttons now work intuitively (enter does not reset the popup, etc).

--- popup.js
+++ popup.js
@@ -1,14 +1,30 @@
-window.onload=function(){
+window.onload= async function(){
+
+    console.log('Popup opened');
+    chrome.storage.local.get("leftCur", (result) => document.getElementById("dropLeft").value = `${result.leftCur}` );
+    chrome.storage.local.get("leftVal", (result) => console.log(`leftVal is ${result.leftVal}`) );
+    chrome.storage.local.get("leftVal", (result) => document.getElementById("leftForm").value = `${result.leftVal}` );
+
+    chrome.storage.local.get("rightCur", (result) => document.getElementById("dropRight").value = `${result.rightCur}` );
+    chrome.storage.local.get("rightVal", (result) => console.log(`rightVal is ${result.rightVal}`) );
+    chrome.storage.local.get("rightVal", (result) => document.getElementById("rightForm").value = `${result.rightVal}` );
+
     document.getElementById("leftForm").addEventListener("submit", leftChange);
+    document.getElementById("rightForm").addEventListener("submit", rightChange);
+
     document.getElementById("leftForm").addEventListener("change", leftChange);
     document.getElementById("rightForm").addEventListener("change", rightChange);
     document.getElementById("dropLeft").addEventListener("change", leftChange);
     document.getElementById("dropRight").addEventListener("change", rightChange);
+
+    leftChange;
+    rightChange;
+    console.log("end of onload")
 }
-// async await- await just stops the function right there while it waits for the API to return.
 
 async function leftChange() {
     var leftVal = document.getElementById("leftForm").value;
+    console.log(`leftVal is ${leftVal}`);
     var leftDrop = document.getElementById("dropLeft");
     var leftCur = leftDrop.options[leftDrop.selectedIndex].value;
     var rightDrop = document.getElementById("dropRight");
@@ -22,8 +38,16 @@ async function leftChange() {
 
     let response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${leftCur}.json?d=${today}`);
     let json = await response.json();
+    
  
     document.getElementById("rightForm").value = leftVal * json[leftCur][rightCur];
+
+    var rightVal = leftVal * json[leftCur][rightCur];
+
+    chrome.storage.local.set({leftCur: `${leftCur}`}, () => console.log("recorded"));
+    chrome.storage.local.set({leftVal: `${leftVal}`}, () => console.log("recorded"));
+    chrome.storage.local.set({rightVal: `${rightVal}`}, () => console.log("recorded"));
+    
 
 }
 async function rightChange() {
@@ -43,6 +67,12 @@ async function rightChange() {
     let json = await response.json();
  
     document.getElementById("leftForm").value = rightVal * json[rightCur][leftCur];
+
+    var leftVal = rightVal * json[rightCur][leftCur];
+    
+    chrome.storage.local.set({rightCur: `${rightCur}`}, () => console.log("recorded"));
+    chrome.storage.local.set({leftVal: `${leftVal}`}, () => console.log("recorded"));
+    chrome.storage.local.set({rightVal: `${rightVal}`}, () => console.log("recorded"));
 }
 
 
