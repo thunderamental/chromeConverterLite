34381c7a825ae6b7d86194ddb5fc3d785b87ed53
King W T <king.tkw.97@gmail.com>
2022-03-02 15:22:12 +1100

    Added reset button; changed formatting with the table borders

--- popup.js
+++ popup.js
@@ -17,12 +17,38 @@ window.onload= async function(){
     document.getElementById("dropLeft").addEventListener("change", leftChange);
     document.getElementById("dropRight").addEventListener("change", rightChange);
 
-    leftChange;
-    rightChange;
+    
+    document.getElementById("clearButton").addEventListener("submit", function(event) {
+        event.preventDefault();
+    }, false);
+    document.getElementById("clearButton").addEventListener("click", hitClear);
+
     console.log("end of onload")
 }
 
+async function hitClear() {
+    console.log("CLEARED")
+    // Can set defaults in options.
+    // chrome.storage.local.clear();
+
+    // chrome.storage.local.set({leftCur: `usd`}, () => console.log("left recorded USD"));
+    chrome.storage.local.set({leftVal: ``}, () => console.log("left recorded value"));
+    // chrome.storage.local.set({rightCur: `aud`}, () => console.log("right recorded AUD"));
+    chrome.storage.local.set({rightVal: ``}, () => console.log("right recorded value"));
+
+
+    chrome.storage.local.get("leftCur", (result) => document.getElementById("dropLeft").value = `${result.leftCur}` );
+    chrome.storage.local.get("leftVal", (result) => console.log(`leftVal is ${result.leftVal}`) );
+    chrome.storage.local.get("leftVal", (result) => document.getElementById("leftForm").value = `${result.leftVal}` );
+
+    chrome.storage.local.get("rightCur", (result) => document.getElementById("dropRight").value = `${result.rightCur}` );
+    chrome.storage.local.get("rightVal", (result) => console.log(`rightVal is ${result.rightVal}`) );
+    chrome.storage.local.get("rightVal", (result) => document.getElementById("rightForm").value = `${result.rightVal}` );
+
+}
+
 async function leftChange() {
+    console.log("leftChange called")
     var leftVal = document.getElementById("leftForm").value;
     console.log(`leftVal is ${leftVal}`);
     var leftDrop = document.getElementById("dropLeft");
@@ -46,7 +72,7 @@ async function leftChange() {
 
     chrome.storage.local.set({leftCur: `${leftCur}`}, () => console.log("recorded"));
     chrome.storage.local.set({leftVal: `${leftVal}`}, () => console.log("recorded"));
-    chrome.storage.local.set({rightVal: `${rightVal}`}, () => console.log("recorded"));
+    chrome.storage.local.set({rightVal: `${rightVal}`}, () => console.log(`leftchange right var recorded as ${rightVal}`));
     
 
 }
